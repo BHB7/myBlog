8bc094f4a9229e49391739064658decca5001716
7z <119857599+BHB7@users.noreply.github.com>
2024-09-29 18:10:56 +0800

    更新 index.vue

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -6,7 +6,7 @@ defineOptions({
 </script>
 <template>
   <myNav></myNav>
-  <div style="background-image:url(https://blog-1312814407.cos.ap-nanjing.myqcloud.com/Screenshot_2022_0203_165533.png); background-attachment: fixed;
+  <div style="background-image:url(https://blog-1312814407.cos.ap-nanjing.myqcloud.com/mmexport1727600601783.jpg); background-attachment: fixed;
     background-size: cover;" class="lg:my-5 w m-auto max-w-5xl w-full px-2 lg:px-0">
     <router-view />
   </div>
