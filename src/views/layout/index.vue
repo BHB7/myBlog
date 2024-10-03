7371d61889e55d76760a2395a325223639039b83
7z <1812287263@qq.com>
2024-10-03 17:58:39 +0800

    背景

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,14 +1,20 @@
 <script setup>
+import { ref } from 'vue'
 import myNav from '@/components/myNav.vue'
 
 defineOptions({
   name: 'LayoutIndex'
 })
+
+const backgroundImageUrl = ref(
+  'https://blog-1312814407.cos.ap-nanjing.myqcloud.com/mmexport1727600601783.jpg'
+)
 </script>
+
 <template>
-  <div class="body-box"></div>
+  <div class="body-box" :style="{ backgroundImage: `url(${backgroundImageUrl})` }"></div>
   <myNav></myNav>
-  <div class="lg:my-5 w m-auto max-w-5xl w-full px-2 lg:px-0">
+  <div class="lg:my-5 m-auto max-w-5xl w-full px-2 lg:px-0">
     <router-view />
   </div>
 </template>
@@ -18,12 +24,12 @@ defineOptions({
   width: 100%;
   height: 100vh;
   position: absolute;
-  z-index: -100;
+  z-index: -1; // 改为 -1 以确保它在内容后面
   top: 0;
   left: 0;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  background-image: url(https://blog-1312814407.cos.ap-nanjing.myqcloud.com/mmexport1727600601783.jpg);
+  background-attachment: fixed; // 固定背景
 }
 </style>
