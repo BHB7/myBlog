f24bf8179203f3cbb3affce89f2cdc0cc2a508a7
7z <1812287263@qq.com>
2024-10-21 08:44:53 +0800

    文章组件样式调整

--- src/components/myArticle.vue
+++ src/components/myArticle.vue
@@ -3,42 +3,25 @@ const props = defineProps({
   // trend 为 'end' 时颠倒子元素的显示顺序，默认为 'start'
   trend: {
     type: String,
-    default: 'start'
+    default: 'end'
   }
 })
 </script>
 
 <template>
   <section
-    :class="[
-      'shadow-xl w-full relative flex h-40 p-2 border-solid rounded-lg overflow-hidden transition-transform duration-300',
-      trend === 'end' ? 'lg:flex-row-reverse flex-col-reverse' : 'lg:flex-row flex-col'
-    ]"
+    class="shadow-xl w-full relative flex h-40 p-2 border-solid rounded-lg overflow-hidden transition-transform duration-300 flex-row"
   >
     <!-- 封面图 -->
-    <div class="lg:w-2/4 w-full object-cover h-full cover-box overflow-hidden rounded-lg">
+    <div class="w-2/4 object-cover h-full cover-box overflow-hidden rounded-lg">
       <slot name="cover"></slot>
     </div>
 
     <!-- 移动端标签 -->
-    <div
-      class="font-light backdrop-blur-sm text-xs p-2 text-white items-center lg:hidden absolute top-0 left-0 bg-opacity-40 bg-zinc-900 rounded-br-lg"
-    >
-      <div class="flex items-center p-1">
-        <span class="iconfont icon-shijian mr-1"></span>
-        <span>更新于：<slot name="time">1984-9-9</slot></span>
-        <!-- 标签 -->
-        <div class="flex mx-2">
-          <slot name="tag">
-            <div class="px-2 py-1 bg-red-500 rounded-md">暂无</div>
-          </slot>
-        </div>
-      </div>
-    </div>
 
     <!-- 描述容器 -->
     <div
-      class="flex-1 w-full overflow-hidden lg:pl-6 pl-4 lg:static lg:h-full absolute bottom-0 h-2/4 bg-gradient-to-b to-transparent"
+      class="flex-1 w-full overflow-hidden lg:pl-6 pl-4 lg:static h-full bottom-0 bg-gradient-to-b to-transparent"
     >
       <!-- 描述 -->
       <article class="">
@@ -49,7 +32,7 @@ const props = defineProps({
           <slot name="desc"></slot>
         </p>
       </article>
-      <div class="font-light text-xs text-white items-center lg:flex hidden">
+      <div class="font-light text-xs text-white items-center flex">
         <span class="iconfont icon-shijian mr-1"></span>
         <span>发布于：<slot name="time">1984-9-9</slot></span>
         <!-- 标签 -->
